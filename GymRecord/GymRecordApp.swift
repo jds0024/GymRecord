aeca76bbf7daf1fc77a6cb70ba2f3961a2ccefd7
Dasan <jds0024@naver.com>
2023-11-04 14:25:09 +0900

    first commit

---
+++ GymRecord/GymRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymRecordApp.swift
+//  GymRecord
+//
+//  Created by 정다산 on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GymRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
